c7dd1731c41dc88bb3dbc82f7f78d23bc6545863
Snoresville <reflectiveflawless@hotmail.com>
2024-05-16 20:25:43 +0930

    Goodbye docs

---
+++ scripts/serve_to_web.bat
@@ -0,0 +1,6 @@
+:: Gets the path of this file since Administrator cmd starts at C:/Windows/system32
+SET mypath=%~dp0
+cd %mypath:~0,-1%
+
+cd ../website
+npm run deploy
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,5 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/snoresville_turbulent_jokers/",
     plugins: [vue()],
+    resolve: {
+        alias: [],
+    },
 });
